bfe2a1477d6e7a763f1da916024fb0c80c305a7b
gmel1199-spec <gmel1199@gmail.com>
2026-08-31 18:12:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.SHATRANJ_CONFIG = {
   supabase: {
     enabled: true,
-    url: "https://zjkxxhsvltihucdacjrv.supabase.co",
+    url: "https://zjxkxhsvltihucdacjrv.supabase.co",
     anonKey: "sb_publishable_bwFGOiJzT_Xv656pLPR8ww_oJxFzSGJ"
   }
 };
