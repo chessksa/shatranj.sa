ca08134c0ca47badd38948fba5091e6a7ae06625
chessksa <gmel1199@gmail.com>
2026-09-03 14:10:45 +0000

    feat: add gender to signup

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="shatranj-saudi-v2";
+const CACHE="shatranj-saudi-v3";
 const ASSETS=["./","./index.html","./styles.css","./app.js","./config.js","./manifest.webmanifest"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
