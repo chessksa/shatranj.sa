49a28078e25d39c6886149d75b9f56ce6489ae0e
chessksa <gmel1199@gmail.com>
2026-09-04 00:30:27 +0300

    test: require cache-safe approved piece paths

--- tests/test_approved_piece_sprite.py
+++ tests/test_approved_piece_sprite.py
@@ -13,11 +13,11 @@ def test_cm_chessboard_uses_approved_piece_sprite_everywhere():
     assert "pieces/staunty.svg" not in prematch
 
 
-def test_approved_sprite_maps_all_twelve_existing_png_pieces():
+def test_approved_sprite_maps_all_twelve_existing_png_pieces_from_page_root():
     sprite = (ROOT / "assets" / "pieces" / "shatranj-approved.svg").read_text(encoding="utf-8")
     for code in ("wk", "wq", "wr", "wb", "wn", "wp", "bk", "bq", "br", "bb", "bn", "bp"):
         assert f'id="{code}"' in sprite
-        assert f'href="{code}.png"' in sprite
+        assert f'href="assets/pieces/{code}.png"' in sprite
 
 
 def test_board_theme_keeps_approved_cream_and_petrol_colors():
