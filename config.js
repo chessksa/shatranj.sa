294cfa9d33ee3f6f4b5497eecbd35b050381c5c3
chessksa <gmel1199@gmail.com>
2026-09-03 09:13:34 +0300

    Load Dubrovnik chess piece theme on play page

--- config.js
+++ config.js
@@ -5,3 +5,10 @@ window.SHATRANJ_CONFIG = {
     anonKey: "sb_publishable_bwFGOiJzT_Xv656pLPR8ww_oJxFzSGJ"
   }
 };
+
+if (typeof document !== 'undefined' && typeof location !== 'undefined' && /(?:^|\/)play\.html$/.test(location.pathname)) {
+  const pieceTheme = document.createElement('link');
+  pieceTheme.rel = 'stylesheet';
+  pieceTheme.href = 'dubrovnik-pieces.css?v=20260903-1';
+  document.head.appendChild(pieceTheme);
+}
