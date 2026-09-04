65f4362e0ba6a285423dbd48fde7ddc7d5272447
chessksa <gmel1199@gmail.com>
2026-09-04 11:31:45 +0000

    fix: embed chess pieces for iOS Safari

--- play-v8.js
+++ play-v8.js
@@ -358,7 +358,7 @@ function ensureBoard(){
       cssClass:'shatranj',
       showCoordinates:false,
       borderType:BORDER_TYPE.none,
-      pieces:{file:'pieces/shatranj-approved-20260904.svg?v=20260904-9',tileSize:40},
+      pieces:{file:'pieces/shatranj-approved-20260904.svg?v=20260904-10',tileSize:40},
       animationDuration:180
     }
   });
