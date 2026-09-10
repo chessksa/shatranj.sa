5bed78f4caeb670bb9026e78e0f05455415e8d4e
chessksa <gmel1199@gmail.com>
2026-09-10 06:01:18 +0300

    chore: bust dotted pull refresh cache

--- site-presence.js
+++ site-presence.js
@@ -1,2 +1,2 @@
-import './pull-to-refresh.js?v=20260910-pull-gold2';
-import './site-presence-base.js?v=20260910-pull-gold2';
+import './pull-to-refresh.js?v=20260910-pull-dots1';
+import './site-presence-base.js?v=20260910-pull-dots1';
