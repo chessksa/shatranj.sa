75f5afd6abfb2e30b4422eb92dbbad3ca8660845
chessksa <gmel1199@gmail.com>
2026-09-10 16:36:02 +0300

    fix: load larger tournament mobile fonts

--- site-presence.js
+++ site-presence.js
@@ -1,2 +1,9 @@
 import './pull-to-refresh.js?v=20260910-pull-dots1';
 import './site-presence-base.js?v=20260910-pull-dots1';
+
+if(document.querySelector('.tournament-table')){
+  const link=document.createElement('link');
+  link.rel='stylesheet';
+  link.href='tournament-mobile-fonts.css?v=20260910-font1';
+  document.head.appendChild(link);
+}
