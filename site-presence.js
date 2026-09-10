d0843885b03433a15770ff56b3a5c75b2e3306d3
chessksa <gmel1199@gmail.com>
2026-09-10 05:53:24 +0300

    chore: bust shared pull refresh cache

--- site-presence.js
+++ site-presence.js
@@ -1,2 +1,2 @@
-import './pull-to-refresh.js?v=20260910-pull-refresh1';
-import './site-presence-base.js?v=20260910-pull-refresh1';
+import './pull-to-refresh.js?v=20260910-pull-gold2';
+import './site-presence-base.js?v=20260910-pull-gold2';
