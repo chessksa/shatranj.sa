3928789bd6fa8603f738b3a27b0155de86acf0ce
chessksa <gmel1199@gmail.com>
2026-09-10 16:43:33 +0300

    fix: bust tournament font stylesheet cache

--- site-presence.js
+++ site-presence.js
@@ -4,6 +4,6 @@ import './site-presence-base.js?v=20260910-pull-dots1';
 if(document.querySelector('.tournament-table')){
   const link=document.createElement('link');
   link.rel='stylesheet';
-  link.href='tournament-mobile-fonts.css?v=20260910-font1';
+  link.href='tournament-mobile-fonts.css?v=20260910-font14';
   document.head.appendChild(link);
 }
