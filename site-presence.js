5926c996afe135348ad181b273d14e6844cd67f6
chessksa <gmel1199@gmail.com>
2026-09-10 20:20:04 +0300

    fix: refresh transformed tournament detail module

--- site-presence.js
+++ site-presence.js
@@ -1,5 +1,5 @@
 import './pull-to-refresh.js?v=20260910-tournament-pull1';
-import './site-presence-base.js?v=20260910-pull-dots1';
+import './site-presence-base.js?v=20260910-tournament-detail-table2';
 
 if(document.querySelector('.tournament-table')){
   document.querySelector('.page-head')?.remove();
