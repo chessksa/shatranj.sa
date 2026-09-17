8accaa9415829501976836129bd83409a74c43f0
chessksa <gmel1199@gmail.com>
2026-09-18 00:15:18 +0300

    Bust desktop tune stylesheet cache

--- v2/home/desktop-board-shell-tune.mjs
+++ v2/home/desktop-board-shell-tune.mjs
@@ -1,4 +1,4 @@
 const css=document.createElement('link');
 css.rel='stylesheet';
-css.href='v2/home/desktop-board-shell-tune.css?v=20260918-size-icons-arrows1';
+css.href='v2/home/desktop-board-shell-tune.css?v=20260918-noarrows-spacing2';
 document.head.appendChild(css);
