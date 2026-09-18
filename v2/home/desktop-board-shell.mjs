6c917595283546cdfc5e20b916184dce38e7ecc8
chessksa <gmel1199@gmail.com>
2026-09-18 04:23:16 +0300

    Load gray move dots

--- v2/home/desktop-board-shell.mjs
+++ v2/home/desktop-board-shell.mjs
@@ -1,4 +1,4 @@
-import { mountInlineComputer, stopInlineComputer } from './inline-computer.mjs?v=20260918-gray-dots1';
+import { mountInlineComputer, stopInlineComputer } from './inline-computer.mjs?v=20260918-gray-dot2';
 if(!document.querySelector('link[data-desktop-board-shell]')){
   const css=document.createElement('link');
   css.rel='stylesheet';
