2b5ede9a17eb6e8ec6661ff1aa444d6af8c7f110
chessksa <gmel1199@gmail.com>
2026-09-18 04:00:36 +0300

    Load computer move options and result colors

--- v2/home/desktop-board-shell.mjs
+++ v2/home/desktop-board-shell.mjs
@@ -1,4 +1,4 @@
-import { mountInlineComputer, stopInlineComputer } from './inline-computer.mjs?v=20260918-last-move1';
+import { mountInlineComputer, stopInlineComputer } from './inline-computer.mjs?v=20260918-move-options-result1';
 if(!document.querySelector('link[data-desktop-board-shell]')){
   const css=document.createElement('link');
   css.rel='stylesheet';
