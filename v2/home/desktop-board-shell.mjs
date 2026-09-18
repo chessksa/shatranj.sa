d27c5ec45ca8dba5990f94c5a1fb1d8ea591886b
chessksa <gmel1199@gmail.com>
2026-09-18 03:44:10 +0300

    Load local Stockfish inline computer mode

--- v2/home/desktop-board-shell.mjs
+++ v2/home/desktop-board-shell.mjs
@@ -1,4 +1,4 @@
-import { mountInlineComputer, stopInlineComputer } from './inline-computer.mjs?v=20260918-start-inline-fix1';
+import { mountInlineComputer, stopInlineComputer } from './inline-computer.mjs?v=20260918-local-stockfish1';
 if(!document.querySelector('link[data-desktop-board-shell]')){
   const css=document.createElement('link');
   css.rel='stylesheet';
